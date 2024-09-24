78530ea61b459060e4d7ed53a5f6f6a8d173b88e
Ej7094 <joshua.bradley@kingsway.school.nz>
2024-09-25 10:24:19 +1200

    Add files via upload

---
+++ # Loops.py
@@ -0,0 +1,21 @@
+# loops and indents
+# Author: Joshua Bradley
+# Date 20 september 2024
+# Version 1 
+
+#TODO:
+    # Get user input (Ask the user for thier name)
+    # Ask the user for 2 numbers
+    # Add the numbers together
+
+# Ask the user for thier name
+name = input("What is your name?")
+print(f"Whats good {name}.") # f stands for format.
+
+num_1 = int(input(f"Whats your favourite number {name}?"))
+num_2 = int(input("Wrong number choose again"))
+
+# add numbers together
+sum = num_1 + num_2
+print(f"The numbers added togehter are equal to {sum}")
+
