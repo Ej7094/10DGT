78c75e7265838d45cb5ad7f950097fd48b243f5a
Ej7094 <joshua.bradley@kingsway.school.nz>
2024-09-25 09:20:59 +1200

    Add files via upload

---
+++ # loops and indents.py
@@ -0,0 +1,19 @@
+# loops and indents
+# Author: Joshua Bradley
+# Date 20 september 2024
+# Version 1 
+
+#for loops. repeat for a set amount of times
+# for start the loop
+# next is the name of the loop
+# in range tells the loop how many times to run
+# the number in the () says how many times to repeat
+for name_of_loop in range(10):
+    print("ha ")
+
+# while loop. runs until condition is met
+keep_going = "" # empty variable
+while keep_going == "":
+    print("Looping")
+    print("Still looping")
+    
