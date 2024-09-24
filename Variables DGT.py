6291b5b88b85614c665a8d66f4f899c6468fbf2a
Ej7094 <joshua.bradley@kingsway.school.nz>
2024-09-25 09:20:05 +1200

    Add files via upload

---
+++ Variables DGT.py
@@ -0,0 +1,27 @@
+# varibles
+# Author: Joshua Bradley
+# Date 20 september 2024
+# Version 1 
+
+# Different types of variables
+# Store a number
+num_1 = 80
+print(num_1)
+
+# Store a string
+name = "Josh"
+print(name)
+name = "aaskdh" # reassessing the name
+print(name)
+
+# do calculations  in a variable  and store the result
+sum = 5+5 # not good practice
+sum = 5 + 5 #follows pep8 python conventions
+print(sum)
+sum = "5 + 5 = 10" # this is a string
+print(sum)
+
+
+#Adding strings together is called a concatenation
+concatenation = name + sum
+print(concatanation)
